b02a8c5079b22dbfafb0c5aca6a77bf399c083db
Nathan Nelson <nnelson19@ewu.edu>
2022-02-16 13:53:02 -0800

    Edited SampleData-> Added GetUniqueSortedListOfStatesGivenCsvRows() and GetAggregatedSortedListOfStatesUsingCsvRows() methods

--- Assignment/Assignment/SampleData.cs
+++ Assignment/Assignment/SampleData.cs
@@ -1,20 +1,66 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Assignment
 {
     public class SampleData : ISampleData
     {
+        private IEnumerable<string> _CsvRows;
+
+        public SampleData()
+        {
+            List<string> rows = new();
+            using (var reader = new StreamReader("People.csv"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string? line = reader.ReadLine();
+                    if(line == null)
+                    {
+                        throw new ArgumentNullException(nameof(line));
+                    }
+                    rows.Add(line);
+                }
+                _CsvRows = rows;
+            }
+        }
+
         // 1.
-        public IEnumerable<string> CsvRows => throw new NotImplementedException();
+        public IEnumerable<string> CsvRows { get { return _CsvRows; } }
 
         // 2.
-        public IEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows() 
-            => throw new NotImplementedException();
+        public IEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows()
+        {
+            List<string> states = new List<string>();
+            foreach(string row in _CsvRows)
+            {
+                string[] array = row.Split(',');
+                states.Add(array[6]);
+            }
+            if(states is null || states.Count == 0)
+            {
+                throw new ArgumentNullException();
+            }
+            states.OrderBy(x => x).Distinct().ToList();          
+            
+            return states;
+        }
 
         // 3.
-        public string GetAggregateSortedListOfStatesUsingCsvRows()
-            => throw new NotImplementedException();
+        public string GetAggregateSortedListOfStatesUsingCsvRows() //This seems easier than what the instructions said to do?????
+        {
+            IEnumerable<string> states = GetUniqueSortedListOfStatesGivenCsvRows();
+
+            string statesString = "";
+
+            foreach(string state in states){
+                statesString += state + " ";
+            }
+            
+            return statesString;
+        }
 
         // 4.
         public IEnumerable<IPerson> People => throw new NotImplementedException();
