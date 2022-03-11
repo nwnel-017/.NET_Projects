f3a335a5a5583018fccabd04cc612fb0602470b3
nwnel-017 <93545905+nwnel-017@users.noreply.github.com>
2022-03-10 22:51:18 -0800

    Add files via upload
    Finished project

---
+++ Assignment7+8/Assignment7+8/js/index.js
@@ -0,0 +1,40 @@
+
+function getJoke(){
+    let jokeText = document.querySelector(".joke");
+    axios
+    ({
+        method: 'get',
+        url: "https://v2.jokeapi.dev/joke/Programming"
+    })
+    .then(function (response){
+        console.log(response);
+
+        let jokeType = response.data.type;
+        // let jokeText = document.querySelector(".joke");
+        console.log(jokeType);
+
+        if(jokeType == "single"){
+            jokeText.innerText = response.data.joke;
+        } else if(jokeType == "twopart") {
+            jokeText.innerText = response.data.setup;
+            let punchline = document.querySelector(".punchline");
+            punchline.style.visibility = 'hidden';
+            punchline.innerText = response.data.delivery;
+
+            setTimeout(revealDelivery, 4000);
+        }
+        else {
+            jokeText.innerText = "Please try again in a few seconds";
+        }
+    })
+    .catch(function (error) 
+   {
+      console.log(console);
+      jokeText.innerText = "Something went wrong, please try again in a few moments";
+   });
+}
+
+function revealDelivery(){
+    let punchline = document.querySelector(".punchline");
+    punchline.style.visibility = 'visible';
+}
